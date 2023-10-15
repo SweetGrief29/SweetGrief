8a9ef50e6a7eaa120974d96fb076036dfe46a307
Sweet Grief <48440794+SweetGrief29@users.noreply.github.com>
2023-10-15 21:07:48 +0700

    Add files via upload

--- Path/costum/script/script.js
+++ Path/costum/script/script.js
@@ -3,7 +3,11 @@ const CheckboxLabel = document.getElementById("CheckboxLabel");
 const NavPC = document.getElementById("NavPc");
 const NavLinkDark = document.querySelectorAll(".nav-link");
 const Nickname = document.getElementById("Nickname");
+const NicknameAndroid = document.getElementById("NicknameAndroid");
 const Sosmeds = document.querySelectorAll(".sosmed");
+const OffcanvasAndroid = document.getElementById("offcanvasAndroid");
+const ResumeBackground = document.getElementById("ResumeBackground");
+const TombolCards = document.querySelectorAll(".TombolCard");
 
 // Menyimpan lokal ketika tombol Darkmode aktif sebelumnya
 function checkDarkMode() {
@@ -14,13 +18,31 @@ function checkDarkMode() {
   if (isDarkModeEnabled) {
     CheckboxLabel.style.backgroundColor = "black";
     NavPC.style.backgroundColor = "#222831";
+    OffcanvasAndroid.style.backgroundColor = "#222831";
     Nickname.style.color = "#EEEEEE";
+    NicknameAndroid.style.color = "#EEEEEE";
+    ResumeBackground.style.backgroundColor = "#393E46"; //Warna Background untuk file resume
+    TombolCards.forEach(function (TombolCard) {
+      //Tombol card file Resume
+      TombolCard.style.color = "#03575c";
+      TombolCard.addEventListener("mouseenter", function () {
+        TombolCard.style.backgroundColor = "#03575c";
+        TombolCard.style.color = "#EEEEEE";
+      });
+      TombolCard.addEventListener("mouseleave", function () {
+        TombolCard.style.backgroundColor = "";
+        TombolCard.style.color = "#03575c";
+      });
+    });
     Sosmeds.forEach(function (sosmed) {
       sosmed.style.color = "#EEEEEE";
       sosmed.style.backgroundColor = "#03575c";
     });
     NavLinkDark.forEach(function (navLink) {
       navLink.style.color = "#03575c";
+      if (!navLink.classList.contains("active")) {
+        navLink.classList.add("whiteelse");
+      }
       if (navLink.classList.contains("active")) {
         navLink.style.color = "#EEEEEE";
         navLink.classList.add("dark");
@@ -32,7 +54,6 @@ function checkDarkMode() {
           navLink.style.color = "#000000";
         }
       });
-
       navLink.addEventListener("mouseleave", function () {
         if (!navLink.classList.contains("active")) {
           navLink.style.backgroundColor = "";
@@ -43,13 +64,34 @@ function checkDarkMode() {
   } else {
     CheckboxLabel.style.backgroundColor = "";
     NavPC.style.backgroundColor = "";
+    OffcanvasAndroid.style.backgroundColor = "";
     Nickname.style.color = "";
+    NicknameAndroid.style.color = "";
+    ResumeBackground.style.backgroundColor = ""; //Warna Background untuk file resume
+    TombolCards.forEach(function (TombolCard) {
+      //Tombol card file Resume
+      TombolCard.style.color = "";
+      TombolCard.addEventListener("mouseenter", function () {
+        TombolCard.style.backgroundColor = "";
+        TombolCard.style.color = "";
+      });
+      TombolCard.addEventListener("mouseleave", function () {
+        TombolCard.style.backgroundColor = "";
+        TombolCard.style.color = "";
+      });
+    });
     Sosmeds.forEach(function (sosmed) {
       sosmed.style.color = "";
       sosmed.style.backgroundColor = "";
     });
     NavLinkDark.forEach(function (navLink) {
       navLink.style.color = "";
+      if (!navLink.classList.contains("active")) {
+        navLink.classList.add("whiteelse");
+      }
+      if (!navLink.classList.contains("active")) {
+        navLink.classList.add("whiteelse");
+      }
       if (navLink.classList.contains("active")) {
         navLink.style.color = "#000000";
         navLink.classList.remove("dark");
@@ -61,7 +103,6 @@ function checkDarkMode() {
           navLink.style.color = "#000000";
         }
       });
-
       navLink.addEventListener("mouseleave", function () {
         if (!navLink.classList.contains("active")) {
           navLink.style.backgroundColor = "";
@@ -84,7 +125,22 @@ checkbox.addEventListener("change", function () {
   if (isDarkModeEnabled) {
     CheckboxLabel.style.backgroundColor = "black";
     NavPC.style.backgroundColor = "#222831";
+    OffcanvasAndroid.style.backgroundColor = "#222831";
     Nickname.style.color = "#EEEEEE";
+    NicknameAndroid.style.color = "#EEEEEE";
+    ResumeBackground.style.backgroundColor = "#393E46"; //Warna Background untuk file resume
+    TombolCards.forEach(function (TombolCard) {
+      //Tombol card file Resume
+      TombolCard.style.color = "#03575c";
+      TombolCard.addEventListener("mouseenter", function () {
+        TombolCard.style.backgroundColor = "#03575c";
+        TombolCard.style.color = "#EEEEEE";
+      });
+      TombolCard.addEventListener("mouseleave", function () {
+        TombolCard.style.backgroundColor = "";
+        TombolCard.style.color = "#03575c";
+      });
+    });
     Sosmeds.forEach(function (sosmed) {
       sosmed.style.color = "#EEEEEE";
       sosmed.style.backgroundColor = "#03575c";
@@ -102,7 +158,6 @@ checkbox.addEventListener("change", function () {
           navLink.style.color = "#000000";
         }
       });
-
       navLink.addEventListener("mouseleave", function () {
         if (!navLink.classList.contains("active")) {
           navLink.style.backgroundColor = "";
@@ -113,13 +168,31 @@ checkbox.addEventListener("change", function () {
   } else {
     CheckboxLabel.style.backgroundColor = "";
     NavPC.style.backgroundColor = "";
+    OffcanvasAndroid.style.backgroundColor = "";
     Nickname.style.color = "";
+    NicknameAndroid.style.color = "";
+    ResumeBackground.style.backgroundColor = ""; //Warna Background untuk file resume
+    TombolCards.forEach(function (TombolCard) {
+      //Tombol card file Resume
+      TombolCard.style.color = "";
+      TombolCard.addEventListener("mouseenter", function () {
+        TombolCard.style.backgroundColor = "";
+        TombolCard.style.color = "";
+      });
+      TombolCard.addEventListener("mouseleave", function () {
+        TombolCard.style.backgroundColor = "";
+        TombolCard.style.color = "";
+      });
+    });
     Sosmeds.forEach(function (sosmed) {
       sosmed.style.color = "";
       sosmed.style.backgroundColor = "";
     });
     NavLinkDark.forEach(function (navLink) {
       navLink.style.color = "";
+      if (!navLink.classList.contains("active")) {
+        navLink.classList.add("whiteelse");
+      }
       if (navLink.classList.contains("active")) {
         navLink.style.color = "#000000";
         navLink.classList.remove("dark");
@@ -131,7 +204,6 @@ checkbox.addEventListener("change", function () {
           navLink.style.color = "#000000";
         }
       });
-
       navLink.addEventListener("mouseleave", function () {
         if (!navLink.classList.contains("active")) {
           navLink.style.backgroundColor = "";
